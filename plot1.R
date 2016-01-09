347460a6dad371b6b602dc0f3a6059af033701fa
aasimwani <wani.aasim@yahoo.com>
2016-01-09 20:53:19 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,6 +1,6 @@
-dataFile <- "~/Desktop/household_power_consumption.txt"
+file_location <- "~/Desktop/household_power_consumption.txt"
 
-data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+data <- read.table(file_location, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
